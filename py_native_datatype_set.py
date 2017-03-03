27ac4dee40b6a0466872e5bff6f6162dc25f2c6b
Jthan.F <andismail7@gmail.com>
2017-03-03 16:50:26 +0800

    fmt

--- py_native_datatype_set.py
+++ py_native_datatype_set.py
@@ -14,11 +14,11 @@ my_set = {1, 2, 3}
 # set of mixed datatypes
 my_set = {1.0, "Hello", (1, 2, 3)}
 # we can make set from a list
-my_set = set([1,2,3,2])
+my_set = set([1, 2, 3, 2])
 #
 # Create an empty set use set() without any argument, cause {} will create an empty dictionary
-print(type({})) # Output: <class 'dict'>
-print(type(set())) # Output: <class 'set'>
+print(type({}))  # Output: <class 'dict'>
+print(type(set()))  # Output: <class 'set'>
 
 # +How to change a set in Python?
 # Sets are mutable .But since they are unordered,indexing here have no meaning.
@@ -26,17 +26,17 @@ print(type(set())) # Output: <class 'set'>
 # We can add single element using the add() method and multiple elements using the update() method.
 # The update() method can take tuples, lists, strings or other sets as its argument. In all cases, duplicates are avoided.
 # initialize my_set
-my_set = {1,3}
+my_set = {1, 3}
 print(my_set)
 # add an element
 my_set.add(2)
-print(my_set) # Output: {1, 2, 3}
+print(my_set)  # Output: {1, 2, 3}
 # add multiple elements
-my_set.update([2,3,4])
-print(my_set) # Output: {1, 2, 3, 4}
+my_set.update([2, 3, 4])
+print(my_set)  # Output: {1, 2, 3, 4}
 # add list and set
-my_set.update([4,5], {1,6,8})
-print(my_set) # Output: {1, 2, 3, 4, 5, 6, 8}
+my_set.update([4, 5], {1, 6, 8})
+print(my_set)  # Output: {1, 2, 3, 4, 5, 6, 8}
 
 # +How to remove elements from a set?
 # A particular item can be removed from set using methods, discard() and remove().
@@ -45,29 +45,29 @@ print(my_set) # Output: {1, 2, 3, 4, 5, 6, 8}
 my_set = {1, 3, 4, 5, 6}
 # discard an element
 my_set.discard(4)
-print(my_set) # Output: {1, 3, 5, 6}
+print(my_set)  # Output: {1, 3, 5, 6}
 # remove an element
 my_set.remove(6)
-print(my_set) # Output: {1, 3, 5}
+print(my_set)  # Output: {1, 3, 5}
 # discard an element not present in my_set
 my_set.discard(2)
-print(my_set) # Output: {1, 3, 5}
+print(my_set)  # Output: {1, 3, 5}
 # remove an element not present in my_set
-my_set.remove(2) # Output: KeyError: 2
+my_set.remove(2)  # Output: KeyError: 2
 #
 # Similarly, we can remove and return an item using the pop() method.
 # Set being unordered, there is no way of determining which item will be popped. It is completely arbitrary.
 # We can also remove all items from a set using clear().
 my_set = set("HelloWorld")
-print(my_set) # Output: set of unique elements
+print(my_set)  # Output: set of unique elements
 # pop an element
-print(my_set.pop()) # Output: random element
+print(my_set.pop())  # Output: random element
 # pop another element
 my_set.pop()
-print(my_set) # Output: random element
+print(my_set)  # Output: random element
 # clear my_set
 my_set.clear()
-print(my_set) #Output: set()
+print(my_set)  # Output: set()
 
 # +Python Set Operations
 # Sets can be used to carry out mathematical set operations like union, intersection,
@@ -78,8 +78,8 @@ print(my_set) #Output: set()
 A = {1, 2, 3, 4, 5}
 B = {4, 5, 6, 7, 8}
 # use | operator
-print(A | B) # Output: {1, 2, 3, 4, 5, 6, 7, 8}
-print(A.union(B))# Output: {1, 2, 3, 4, 5, 6, 7, 8}
+print(A | B)  # Output: {1, 2, 3, 4, 5, 6, 7, 8}
+print(A.union(B))  # Output: {1, 2, 3, 4, 5, 6, 7, 8}
 #
 # -Set Intersection
 # Intersection of A and B is a set of elements that are common in both sets.
@@ -92,9 +92,9 @@ print(A.intersection(B))
 # Similarly, B - A is a set of element in B but not in A.
 # Difference is performed using - operator. Same can be accomplished using the method difference().
 print(A - B)
-print(A.difference(B)) # Output: {1, 2, 3}
+print(A.difference(B))  # Output: {1, 2, 3}
 print(B - A)
-print(B.difference(A)) # Output: {6, 7, 8}
+print(B.difference(A))  # Output: {6, 7, 8}
 #
 # -Set Symmetric Difference
 # Symmetric Difference of A and B is a set of elements in both A and B except those that are common in both.
@@ -103,23 +103,23 @@ print(A ^ B)
 print(A.symmetric_difference(B))
 
 # +Python Set Methods
-#add()	Add an element to a set
-#clear()	Remove all elements form a set
-#copy()	Return a shallow copy of a set
-#difference()	Return the difference of two or more sets as a new set
-#difference_update()	Remove all elements of another set from this set
-#discard()	Remove an element from set if it is a member. (Do nothing if the element is not in set)
-#intersection()	Return the intersection of two sets as a new set
-#intersection_update()	Update the set with the intersection of itself and another
-#isdisjoint()	Return True if two sets have a null intersection
-#issubset()	Return True if another set contains this set
-#issuperset()	Return True if this set contains another set
-#pop()	Remove and return an arbitary set element. Raise KeyError if the set is empty
-#remove()	Remove an element from a set. If the element is not a member, raise a KeyError
-#symmetric_difference()	Return the symmetric difference of two sets as a new set
-#symmetric_difference_update()	Update a set with the symmetric difference of itself and another
-#union()	Return the union of sets in a new set
-#update()	Update a set with the union of itself and others
+# add()	Add an element to a set
+# clear()	Remove all elements form a set
+# copy()	Return a shallow copy of a set
+# difference()	Return the difference of two or more sets as a new set
+# difference_update()	Remove all elements of another set from this set
+# discard()	Remove an element from set if it is a member. (Do nothing if the element is not in set)
+# intersection()	Return the intersection of two sets as a new set
+# intersection_update()	Update the set with the intersection of itself and another
+# isdisjoint()	Return True if two sets have a null intersection
+# issubset()	Return True if another set contains this set
+# issuperset()	Return True if this set contains another set
+# pop()	Remove and return an arbitary set element. Raise KeyError if the set is empty
+# remove()	Remove an element from a set. If the element is not a member, raise a KeyError
+# symmetric_difference()	Return the symmetric difference of two sets as a new set
+# symmetric_difference_update()	Update a set with the symmetric difference of itself and another
+# union()	Return the union of sets in a new set
+# update()	Update a set with the union of itself and others
 
 # +Other Set Operations
 #
@@ -127,9 +127,9 @@ print(A.symmetric_difference(B))
 # We can test if an item exists in a set or not, using the keyword in.
 my_set = set("apple")
 # check if 'a' is present
-print('a' in my_set) # Output: True
+print('a' in my_set)  # Output: True
 # check if 'p' is present
-print('p' not in my_set) # Output: False
+print('p' not in my_set)  # Output: False
 #
 # -Iterating Through a Set
 for letter in set("apple"):
@@ -138,14 +138,14 @@ for letter in set("apple"):
 # -Built-in Functions with Set
 # Built-in functions like all(), any(), enumerate(), len(), max(), min(), sorted(), sum() etc.
 # are commonly used with set to perform different tasks.
-#all()	Return True if all elements of the set are true (or if the set is empty).
-#any()	Return True if any element of the set is true. If the set is empty, return False.
-#enumerate()	Return an enumerate object. It contains the index and value of all the items of set as a pair.
-#len()	Return the length (the number of items) in the set.
-#max()	Return the largest item in the set.
-#min()	Return the smallest item in the set.
-#sorted()	Return a new sorted list from elements in the set(does not sort the set itself).
-#sum()	Retrun the sum of all elements in the set.
+# all()	Return True if all elements of the set are true (or if the set is empty).
+# any()	Return True if any element of the set is true. If the set is empty, return False.
+# enumerate()	Return an enumerate object. It contains the index and value of all the items of set as a pair.
+# len()	Return the length (the number of items) in the set.
+# max()	Return the largest item in the set.
+# min()	Return the smallest item in the set.
+# sorted()	Return a new sorted list from elements in the set(does not sort the set itself).
+# sum()	Retrun the sum of all elements in the set.
 
 # +Python Frozenset
 # Frozenset is a new class that has the characteristics of a set, but its elements cannot be changed once assigned.
@@ -158,23 +158,3 @@ for letter in set("apple"):
 # This datatype supports methods like copy(), difference(), intersection(), isdisjoint(), issubset(), issuperset(),
 # symmetric_difference() and union().
 # Being immutable it does not have method that add or remove elements.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
